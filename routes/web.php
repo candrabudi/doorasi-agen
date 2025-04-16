24f6716ef49f3408ef3f5619d30d63149a527fad
muhbase <67451498+takanesachio@users.noreply.github.com>
2025-04-17 00:14:47 +0700

    improve route auth

--- routes/web.php
+++ routes/web.php
@@ -19,42 +19,59 @@ Route::get('/get-distributors', [Accordiancontroller::class, 'getDistributorsByR
 Route::get('/list-of-area', [AreaController::class, 'listOfArea']);
 
 
-Route::get('/', function () {
-    return view('welcome');
+// Protected routes with auth middleware
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    
+    // Route tanpa auth - untuk insert area (jika hanya digunakan sekali dan internal, bisa juga dihapus dari route publik)
+    Route::prefix('insert')->group(function () {
+        Route::get('/province', [AreaController::class, 'insertProvince']);
+        Route::get('/regency', [AreaController::class, 'insertRegency']);
+        Route::get('/district', [AreaController::class, 'insertDistrict']);
+        Route::get('/village', [AreaController::class, 'insertVillage']);
+    });
+    // User Routes
+    Route::prefix('users')->name('users.')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('index');
+        Route::get('/create', [UserController::class, 'create'])->name('create');
+        Route::post('/store', [UserController::class, 'store'])->name('store');
+        Route::get('/{a}/edit', [UserController::class, 'edit'])->name('edit');
+        Route::put('/{a}/update', [UserController::class, 'update'])->name('update');
+        Route::delete('/{a}/destroy', [UserController::class, 'destroy'])->name('destroy');
+        Route::patch('/{id}/status', [UserController::class, 'updateStatus'])->name('updateStatus');
+    });
+
+    // Distributor Routes
+    Route::prefix('distributors')->name('distributors.')->group(function () {
+        Route::get('/', [DistributorController::class, 'index'])->name('index');
+        Route::get('/create', [DistributorController::class, 'create'])->name('create');
+        Route::post('/store', [DistributorController::class, 'store'])->name('store');
+        Route::get('/{a}/edit', [DistributorController::class, 'edit'])->name('edit');
+        Route::put('/{a}/update', [DistributorController::class, 'update'])->name('update');
+        Route::delete('/{a}/destroy', [DistributorController::class, 'destroy'])->name('destroy');
+        Route::patch('/{id}/status', [DistributorController::class, 'updateStatus'])->name('updateStatus');
+    });
+
+    // Shipment Routes
+    Route::prefix('shipments')->name('shipments.')->group(function () {
+        Route::get('/', [ShipmentController::class, 'index'])->name('index');
+        Route::post('/store', [ShipmentController::class, 'store'])->name('store');
+        Route::get('/{id}/edit', [ShipmentController::class, 'edit'])->name('edit');
+        Route::post('/{id}/update', [ShipmentController::class, 'update'])->name('update');
+        Route::delete('/{id}/delete', [ShipmentController::class, 'destroy'])->name('destroy');
+    });
+
+    // Marketplace Routes
+    Route::prefix('marketplaces')->name('marketplaces.')->group(function () {
+        Route::get('/', [MarketPlaceController::class, 'index'])->name('index');
+        Route::get('/create', [MarketPlaceController::class, 'create'])->name('create');
+        Route::post('/store', [MarketPlaceController::class, 'store'])->name('store');
+        Route::get('/edit/{id}', [MarketPlaceController::class, 'edit'])->name('edit');
+        Route::put('/update/{id}', [MarketPlaceController::class, 'update'])->name('update');
+        Route::delete('/destroy/{id}', [MarketPlaceController::class, 'destroy'])->name('destroy');
+    });
+
 });
-
-Route::get('/insert-province', [AreaController::class, 'insertProvince']);
-Route::get('/insert-regency', [AreaController::class, 'insertRegency']);
-Route::get('/insert-district', [AreaController::class, 'insertDistrict']);
-Route::get('/insert-village', [AreaController::class, 'insertVillage']);
-
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users/store', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/{a}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{a}/update', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{a}/destroy', [UserController::class, 'destroy'])->name('users.destroy');
-Route::patch('/users/{id}/status', [UserController::class, 'updateStatus'])->name('users.updateStatus');
-
-
-Route::get('/distributors', [DistributorController::class, 'index'])->name('distributors.index');
-Route::get('/distributors/create', [DistributorController::class, 'create'])->name('distributors.create');
-Route::post('/distributors/store', [DistributorController::class, 'store'])->name('distributors.store');
-Route::get('/distributors/{a}/edit', [DistributorController::class, 'edit'])->name('distributors.edit');
-Route::put('/distributors/{a}/update', [DistributorController::class, 'update'])->name('distributors.update');
-Route::delete('/distributors/{a}/destroy', [DistributorController::class, 'destroy'])->name('distributors.destroy');
-Route::patch('/distributors/{id}/status', [DistributorController::class, 'updateStatus'])->name('distributors.updateStatus');
-
-
-Route::get('/shipments', [ShipmentController::class, 'index'])->name('shipments.index');
-Route::post('/shipments/store', [ShipmentController::class, 'store'])->name('shipments.store');
-Route::get('/shipments/{id}/edit', [ShipmentController::class, 'edit'])->name('shipments.edit');
-Route::post('/shipments/{id}/update', [ShipmentController::class, 'update'])->name('shipments.update');
-Route::delete('/shipments/{id}/delete', [ShipmentController::class, 'destroy'])->name('shipments.destroy');
-
-Route::get('/marketplaces', [MarketPlaceController::class, 'index'])->name('marketplaces.index');
-Route::get('/marketplaces/create', [MarketPlaceController::class, 'create'])->name('marketplaces.create');
-Route::post('/marketplaces/store', [MarketPlaceController::class, 'store'])->name('marketplaces.store');
-Route::get('/marketplaces/edit/{id}', [MarketPlaceController::class, 'edit'])->name('marketplaces.edit');
-Route::put('/marketplaces/update/{id}', [MarketPlaceController::class, 'update'])->name('marketplaces.update');
-Route::delete('/marketplaces/destroy/{id}', [MarketPlaceController::class, 'destroy'])->name('marketplaces.destroy');
