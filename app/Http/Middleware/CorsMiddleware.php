a620ae076cb33c5036911f88141615ba5336e921
muhbase <67451498+takanesachio@users.noreply.github.com>
2025-04-17 09:57:04 +0700

    add cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -19,15 +19,17 @@ class CorsMiddleware
         // Jika permintaan adalah OPTIONS, kirimkan respons tanpa melanjutkan ke logika aplikasi
         if ($request->getMethod() == "OPTIONS") {
             return response('', 200)
-                ->header('Access-Control-Allow-Origin', 'https://doorasi.com') // Ganti dengan domain yang diizinkan
+                ->header('Access-Control-Allow-Origin', 'https://doorasi.com')  // Ganti dengan domain yang diizinkan
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization');
+                ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization')
+                ->header('Access-Control-Allow-Credentials', 'true');  // Menambahkan support untuk kredensial
         }
 
         // Menambahkan header CORS untuk permintaan lainnya
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'https://doorasi.com') // Ganti dengan domain yang diizinkan
+            ->header('Access-Control-Allow-Origin', 'https://doorasi.com')  // Ganti dengan domain yang diizinkan
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Authorization')
+            ->header('Access-Control-Allow-Credentials', 'true');  // Menambahkan support untuk kredensial
     }
 }
